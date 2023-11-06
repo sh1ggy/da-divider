e44d87085091dab2ec16c3c00fa7d7ee5a470b79
sh1ggy <tyronewessnolasco@gmail.com>
2023-11-06 16:34:24 +1000

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PlaceComponent } from './place/place.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlaceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/place/place.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place',
+  template: `
+    <p>
+      place works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PlaceComponent {
+
+}
