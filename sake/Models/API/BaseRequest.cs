563320d924368eeb76f4a7cd297f97ea460aa517
sh1ggy <tyronewessnolasco@gmail.com>
2024-07-19 08:11:37 +1000

    skeletonui & express TS init

--- sake/Models/API/BaseRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Divider.ApiModels;
-
-public class BaseRequest
-{
-  [Range(1, int.MaxValue, ErrorMessage = "CreatedUserId is required")]
-  public int UserCreatedId { get; set; }
-}
--- soju/src/prettier.config.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  singleQuote: false,
-  plugins: ["prettier-plugin-tailwindcss"],
-};
