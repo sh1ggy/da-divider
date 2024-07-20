d6f8f726e15a272f5b0b571e1a9dd8fd6aa5557b
sh1ggy <tyronewessnolasco@gmail.com>
2024-07-20 11:35:13 +1000

    working mongodb init

--- sake/server.ts
+++ sake/server.ts
@@ -1,12 +1,31 @@
+require("dotenv").config();
+
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import test from "./routes/test";
+import { Db } from "mongodb";
+import { connectToDatabase } from "./db/connection";
+import { Server, createServer } from "http";
 
 const app: Express = express();
+const http: Server = createServer(app);
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+export let db: Db;
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+app.use(cors());
+app.use(express.json());
+app.use("/test", test);
+
+async function main() {
+  db = (await connectToDatabase()).db;
+  http.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+  });
+}
+
+main();
