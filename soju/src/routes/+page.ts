d334f86fc3e21950ee18d463d0014e1e203372ae
sh1ggy <tyronewessnolasco@gmail.com>
2024-09-23 16:41:25 +1000

    Rename contact.ts for proper casing

--- soju/src/routes/+page.ts
+++ soju/src/routes/+page.ts
@@ -1,15 +1,13 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
-	let test: any = undefined;
+	let test: any | undefined = undefined;
 
 	const url = 'http://localhost:3000/groups';
 	await fetch(url, { method: 'GET' })
 		.then((res) => {
-			console.log(res);
 			return res.json();
 		})
 		.then((data) => {
-			console.log(data);
 			test = data;
 		});
 	return {test: test};
