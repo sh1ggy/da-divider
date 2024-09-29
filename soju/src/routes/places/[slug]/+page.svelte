d6dd25db6117c1ad4031b22a8de56dab0aca98b9
sh1ggy <tyronewessnolasco@gmail.com>
2024-09-30 09:17:40 +1000

    Rendering items in place form

--- soju/src/routes/places/[slug]/+page.svelte
+++ soju/src/routes/places/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import type { Place, PlaceContact } from '../../../types/types.js';
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+	import type { Item, Place, PlaceContact } from '../../../types/types.js';
 
 	const groupId = '66a80e0c312e1ebdd11ed13f';
 
@@ -8,35 +9,72 @@
 
 	let place: Place;
 	let contacts: PlaceContact[];
-
-	if (data.place) place = data.place;
+	let items: Item[];
+	if (data.place) {
+		place = data.place;
+		items = place.items;
+	}
 	if (data.contacts) contacts = data.contacts;
 </script>
 
 <div class="container h-full mx-auto flex flex-col gap-6 justify-center items-center">
 	<button on:click={() => goto('/places')} class="btn variant-soft-primary">Back to Places</button>
-	{#if place !== undefined}
-		<form method="POST" class="flex flex-col items-center gap-6 rounded-lg bg-slate-800 p-12">
-			<h1 class="mr-auto ml-auto">Edit Place</h1>
-			<label class="label">
-				Name
-				<input
-					name="name"
-					type="text"
-					placeholder={place.name}
-					class="input text-sm variant-form-material"
-				/>
-			</label>
-      <div class="flex gap-3">
+	<div class="flex flex-col items-center gap-6 rounded-lg bg-slate-800 p-12">
+		{#if place !== undefined}
+			<form method="POST" class="flex flex-col items-center gap-6 rounded-lg bg-slate-800">
+				<h3 class="mr-auto ml-auto h3">Edit Place</h3>
+				<code class="code">{place._id}</code>
+				<label class="label">
+					Name
+					<input
+						name="name"
+						type="text"
+						placeholder={place.name}
+						class="input text-sm variant-form-material"
+					/>
+				</label>
+				<div class="flex gap-3">
+					{#each contacts as contact}
+						<span class="chip variant-glass-surface">{contact.name}</span>
+					{/each}
+				</div>
+				<button type="submit" class="btn variant-glass-primary w-full">Submit</button>
+			</form>
 
-        {#each contacts as contact}
-          <span class="badge badge-glass">{contact.name}</span>
-        {/each}
-      </div>
-			<button type="submit" class="btn variant-glass-primary w-full">Submit</button>
-			<code class="code">{place._id}</code>
-		</form>
-	{/if}
+			<Accordion>
+				<AccordionItem open>
+					<!-- <svelte:fragment slot="lead">(icon)</svelte:fragment> -->
+					<svelte:fragment slot="summary"><h4 class="h4">Items</h4></svelte:fragment>
+					<svelte:fragment slot="content">
+						<div class="grid grid-cols-2 gap-3">
+							{#if items !== undefined}
+								{#each items as item}
+									<div class="card p-4 gap-3 flex flex-col">
+										<div class="flex">
+											<p>{item.name}</p>
+											<p class="ml-auto badge mb-auto variant-soft-primary">${item.price}</p>
+										</div>
+										<div class="flex gap-1 mt-auto">
+											<button class="btn btn-sm text-xs variant-ghost-warning">edit</button>
+											<button class="btn btn-sm text-xs variant-ghost-error">delete</button>
+										</div>
+									</div>
+								{/each}
+								<div class="col-span-2 gap-3 grid rounded-lg badge-glass p-3">
+									<h5 class="font-bold h5">Add Item</h5>
+									<form method="POST" class="flex flex-col gap-3">
+										<input name="itemName" type="text" placeholder="name" class="input" />
+										<input name="price" type="number" placeholder="price" class="input" />
+										<button type="submit" class="w-full text-sm btn variant-filled-primary">Add Item</button>
+									</form>
+								</div>
+								{/if}
+						</div>
+					</svelte:fragment>
+				</AccordionItem>
+			</Accordion>
+		{/if}
+	</div>
 	<button on:click={() => console.log('delete')} class="btn variant-filled-error"
 		>Delete Place</button
 	>
